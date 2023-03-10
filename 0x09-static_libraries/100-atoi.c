7b4801f67631ab5889ba4155b134a9a14066ab82
Your Name <you@example.com>
2023-03-10 11:39:09 -0800

    A library is not a luxury but one of the necessities of life

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,10 +1,9 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
+int _atoi(char *s)
 
-char *_strchr(char *s, char c)
 {
-    return strchr(s, c);
+    return atoi(s);
 }
