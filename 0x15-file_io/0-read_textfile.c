c62ba84fd9643b04a090249131008f640b30e288
Emmy Kenduiwa <ckenduiwa9876@gmail.com>
2023-04-14 02:22:19 -0700

    Tread lightly, she is near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * read_textfile - Read text fileprintto STDOUT
+ * @filename: file being read
+ * @letters: number of letters to be read
+ * Return: w-number of bytes read and printed
+ * 0 when function is NULL
+ */
+
 size_t read_textfile(const char *filename, size_t letters)
 {
 	FILE *fp;
@@ -11,12 +19,12 @@ size_t read_textfile(const char *filename, size_t letters)
 	if (fp == NULL)
 		return (0);
 
-	buf = (char*)malloc(sizeof(char) * letters);
+	buf = (char *)malloc(sizeof(char) * letters);
 	if (buf == NULL)
 		return (0);
 
 	n = fread(buf, sizeof(char), letters, fp);
-	fwrite(buf, sizeof(char),n, stdout);
+	fwrite(buf, sizeof(char), n, stdout);
 
 	free(buf);
 	fclose(fp);
